2801e0d25c7992315fabc6c26a215c29e2020f17
Gilson Cosme de Vasconcelos Junior <65792764+GilsonJrr@users.noreply.github.com>
2022-02-21 09:29:45 -0300

    Timer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import './App.css';
+
 import Blogo from '../src/img/Blogo.png'
 
 import api from './services/api';
@@ -8,24 +9,39 @@ import api from './services/api';
 function App() {
 
   const [advice, setAdvice] = useState ([]);
+  const [show, setShow] = useState (false);
 
   const refresh = () => {
-    api
+    
+    async function LoadAdvice (){
+      await api
       .get("advice")
       .then((response) => setAdvice(response.data))
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
+    }
+
+    LoadAdvice();
+
   };
   
   useEffect(refresh, []);
+
+  setTimeout(() => {
+    setShow(true)
+  }, 500);
 
   return (
     <div className="App">
       <div className='Back'>
 
-        <p1>ADVICE #{advice.slip.id}</p1>
-        <p2>" {advice.slip.advice} "</p2>
+        { show &&
+          <div className='Titles'>
+            <p1>ADVICE #{advice.slip.id}</p1>
+            <p2>" {advice.slip.advice} "</p2>
+          </div>
+        }
 
         <div className='Lines'>
           <div className='line'/>
@@ -35,7 +51,7 @@ function App() {
         </div>
 
         <button onClick={refresh}>
-          <img src={Blogo}/>
+          <img src={Blogo} alt="diceLogo" />
         </button>
         
       </div>
